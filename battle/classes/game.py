499a74e0b7d377ded4fd8860bfab068358468f3c
Vishrut Jha <vishrutkmr7@gmail.com>
2019-05-31 22:18:46 +0530

    RPG Script 2

--- battle/classes/game.py
+++ battle/classes/game.py
@@ -2,14 +2,15 @@ import random
 
 
 class bcolors:
-    HEADER = '\033[95n'
-    OKBLUE = '\033[94n'
-    OKGREEN = '\033[92n'
-    WARNING = '\033[93n'
-    FAIL = '\033[91n'
-    ENDC = '\033[0n'
-    BOLD = '\033[1n'
-    UNDERLINE = '\033[4n'
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
 
 class Person:
     def __init__(self, hp, mp, atk, df, magic):
@@ -22,3 +23,53 @@ class Person:
         self.df = df
         self.magic = magic
         self.actions = ['Attack', 'Magic']
+
+    def generate_damage(self):
+        return random.randrange(self.atkl, self.atkh)
+
+    def generate_spell_damage(self, i):
+        mgl = self.magic[i]['dmg'] - 5
+        mgh = self.magic[i]['dmg'] + 5
+        return random.randrange(mgl, mgh)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+
+    # Utility Functions
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxHp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxMp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def get_spell_name(self, i):
+        return self.magic[i]['name']
+
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]['cost']
+
+    def choose_action(self):
+        i = 1
+        print('Action:')
+        for item in self.actions:
+            print(str(i) + ':', item)
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+        print('Magic:')
+        for spell in self.magic:
+            print(str(i) + ':', spell['name'], '(cost:', str(spell['mp']), ')')
+            i += 1
