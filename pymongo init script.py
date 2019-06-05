bd1e9f37b9203f22e385575918ccf120a6f058fe
Vishrut Jha <vishrutkmr7@gmail.com>
2019-06-05 21:42:54 +0530

    PyMongo

--- pymongo init script.py
+++ pymongo init script.py
@@ -1,15 +1,45 @@
 import pymongo
+import datetime
 from pymongo import MongoClient
 
 
-myClient = MongoClient()
+client = MongoClient()
 # client = MongoClient("localhost", 27017)
 # client = MongoClient('mongodb://localhost:27017/')
 
-db = myClient.mydb
+db = client.mydb
 # db = myClient["mydb"]
 
-users = db.users
+Users = db.users
 user1 = {"username": "vishrutjha10", "password": "Reallysecure445", "favorite_number": 710, "hobbies": ['python', 'soccer']}
-user_id = users.insert_one(user1).inserted_id
+user_id = Users.insert_one(user1).inserted_id
 print(user_id)
+
+# bulk insert
+users = [{"username": "third", "password": "duh12345678"}, {"username": "fourth", "password": "duhBE12345678"},
+         {"username": "5th", "password": "FOYTH_duh12345678"}]
+
+inserted = Users.insert_many(users)
+print(inserted.inserted_ids)
+
+# counting documents
+print("Documents", Users.find().count())
+print("710:", Users.find({"favorite_number": 710}).count())
+print("710 and vishrutjha10", Users.find({"favorite_number": 710, "username": "vishrutjha10"}).count())
+# count is deprecated
+
+current_date = datetime.datetime.now()
+print(current_date)
+old_date = datetime.datetime(2009, 8, 28)
+uid = Users.insert({'username': 'ids_fotowale', 'dob': old_date, 'today': current_date})
+print(">=", current_date, ":", Users.find({'today': {"$gte": old_date}}).count())
+# gt: Greater than, gte: >=, lt: <, lte: <=, $exists: boolean true
+print(Users.find({'today': {'$exists': True}}).count())
+print("!= ids_fotowale:", Users.find({"username": {"$ne": "ids_fotowale"}}).count())
+
+# Indexing for SCALABILITY
+# index for username
+# db.users.create_index([("username", pymongo.ASCENDING)], unique=True)
+# db.users.create_index([("username", pymongo.ASCENDING)])
+print("Index unique", db.users.create_index([("username", pymongo.ASCENDING)], unique=True))
+print("index not unique", db.users.create_index([("username", pymongo.ASCENDING)]))
--- udemy.py
+++ udemy.py
@@ -1,23 +1,44 @@
+import datetime
+import pymongo
+from pymongo import MongoClient
+
+
 def stringify(name = "Jonas", age = "Unknown"):
     print(name, "is walking for", age, "years")
     # Default Arguments
 
-stringify(27,2)
+
+stringify(str(27), str(2))
 stringify("Vishrut", 22)
 stringify(True, None)
-stringify(age="545", name="Nicholas Flamel") #KeyWord Arguments
+stringify(age="545", name="Nicholas Flamel")
+# KeyWord Arguments
 
-#Infinite Arguments
+
+# Infinite Arguments
 def infinit(*people):
     for person in people:
         print("This person is", person)
 
+
 infinit("Jason", "Mike", "Ashley", "Vishrut", "Joanne")
+
 
 def maths(n1, n2):
     return n1 + n2
+
 
 m1 = maths(12, 3)
 m2 = maths(23, 34)
 
 print("Sum1:", m1, "Sum2:", m2)
+
+# Date Time
+current_date = datetime.datetime.now()
+print(current_date)
+old_date = datetime.datetime(2009, 8, 28)
+client = MongoClient()
+db = client.mydb
+Users = db.Users
+uid = Users.insert_one({'username': 'ids_fotowale', 'dob': old_date, 'today': current_date})
+print(uid.inserted_id)
