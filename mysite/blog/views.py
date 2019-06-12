c743d6257e7700633f996ee9fab9800be4a92eac
Vishrut Jha <vishrutkmr7@gmail.com>
2019-06-12 22:46:59 +0530

    Django1/2

---
+++ mysite/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+def post(request):
+    return HttpResponse("Single post pg.")
