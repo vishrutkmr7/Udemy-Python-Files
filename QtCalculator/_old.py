e1bb89069845610e795875782dadc16f13ce3530
Vishrut Jha <vishrutkmr7@gmail.com>
2019-06-15 21:29:23 +0530

    PyQt and QtCalculator

---
+++ QtCalculator/_old.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QApplication, QHBoxLayout, QVBoxLayout, QPushButton, QLabel, QLineEdit)
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+        self.counter = 0
+
+    def init_ui(self):
+        self.text_label = QLabel("No name entered. Can't do anything yet")
+        self.label = QLabel("Name: ")
+        self.name_input = QLineEdit()
+        self.button = QPushButton("Clicked: 0")
+
+        self.button.setText("Set Name")
+        self.button.clicked.connect(self.alterName)
+
+        h = QHBoxLayout()
+        h.addWidget(self.label)
+        h.addWidget(self.name_input)
+
+        v = QVBoxLayout()
+        v.addWidget(self.text_label)
+        v.addLayout(h)
+        v.addWidget(self.button)
+
+        self.setLayout(v)
+        self.setWindowTitle("Nothing has been clicked")
+        self.show()
+
+    def alterName(self):
+        inputted_text = self.name_input.text()
+        our_string = "You've entered: " + inputted_text
+        self.text_label.setText(our_string)
+        self.setWindowTitle(inputted_text + "'s Window")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
---
+++ QtCalculator/main.py
@@ -0,0 +1,72 @@
+import sys, math
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class Button:
+    def __init__(self, text, results):
+        self.b = QPushButton(str(text))
+        self.text = text
+        self.results = results
+        self.b.clicked.connect(lambda: self.handleInput(self.text))
+
+    def handleInput(self, v):
+        if v == "=":
+            res = eval(self.results.text())
+            self.results.setText(str(res))
+        elif v == 'AC':
+            self.results.setText("")
+        elif v == '√':
+            value = float(eval(self.results.text()))
+            self.results.setText(str(math.sqrt(value)))
+        elif v == "⌫":
+            current_value = self.results.text()
+            self.results.setText(current_value[:-1])
+        else:
+            current_value = self.results.text()
+            new_value = current_value + str(v)
+            self.results.setText(new_value)
+
+
+class Application(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QtCalculator")
+        self.CreateApp()
+
+    def CreateApp(self):
+        # Grid Layout
+        grid = QGridLayout()
+        results = QLineEdit()
+        grid.addWidget(results, 0, 0, 1, 4)
+        buttons = ["AC", "⌫", "√", "/",
+                   7, 8, 9, "*",
+                   4, 5, 6, "-",
+                   1, 2, 3, "+",
+                   0, ".", "="]
+        row = 1
+        col = 0
+        for button in buttons:
+            if col > 3:
+                col = 0
+                row += 1
+
+            buttonObject = Button(button, results)
+
+            if button == 0:
+                grid.addWidget(buttonObject.b, row, col, 1, 2)
+                col += 1
+            else:
+                grid.addWidget(buttonObject.b, row, col, 1, 1)
+
+            col += 1
+
+        self.setLayout(grid)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Application()
+    sys.exit(app.exec_())
